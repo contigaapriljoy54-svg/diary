d492778612ddbee4f2bbb2035af38b0b5bad29f2
April Joy Contiga <YOUR_GITHUB_EMAIL>
2026-02-13 14:42:43 +0800

    new commit

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -3,14 +3,14 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class Users {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column()
-  name: string;
+  name!: string;
 
   @Column({ unique: true })
-  email: string;
+  email!: string;
 
   @Column()
-  password: string;
+  password!: string;
 }
